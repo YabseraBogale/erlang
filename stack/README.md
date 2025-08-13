0041d9e0f706d42c772dda6551db820f66fd50d1
YabseraBogale <yabserapython@gmail.com>
2025-08-13 20:20:43 +0300

    ok

---
+++ stack/README.md
@@ -0,0 +1,9 @@
+stack
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ stack/src/stack_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc stack public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(stack_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    stack_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
